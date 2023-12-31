ebdc5fcab1f4fb66fd8547771d9d2acaef829509
wangxing <xing.wang@xforwardai.com>
2023-12-31 12:05:59 +0530

    feat: 静态资源的使用

--- lib/pages/find/find_page.dart
+++ lib/pages/find/find_page.dart
@@ -11,6 +11,9 @@ class _FindPageState extends State<FindPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: Container(
+        child: const Image(image: AssetImage('images/pengyouquan.jpeg')),
+      ),
       appBar: AppBar(
         title: const Text('发现页面'),
       ),
