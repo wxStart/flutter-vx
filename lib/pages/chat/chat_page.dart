d93ee9409fe920a00cc80c4f9f5e3e850b4f5c1d
wangxing <xing.wang@xforwardai.com>
2024-01-04 15:06:57 +0800

    fix： 获取借口数据

--- lib/pages/chat/chat_page.dart
+++ lib/pages/chat/chat_page.dart
@@ -1,4 +1,25 @@
+import 'dart:convert';
+import 'dart:ffi';
 import 'package:flutter/material.dart';
+import 'package:flutter_vx/common/iconfont.dart';
+import 'package:flutter_vx/const.dart';
+import 'package:http/http.dart' as http;
+
+class Chat {
+  final String name;
+  final String message;
+  final String imageUrl;
+
+  Chat({required this.name, required this.message, required this.imageUrl});
+
+  factory Chat.fromMap(Map map) {
+    return Chat(
+      name: map['chat_name'],
+      message: map['message'],
+      imageUrl: map['chat_url'],
+    );
+  }
+}
 
 class ChatPage extends StatefulWidget {
   const ChatPage({super.key});
@@ -8,30 +29,109 @@ class ChatPage extends StatefulWidget {
 }
 
 class _ChatPageState extends State<ChatPage> {
+  Future<List<Chat>> geDatas() async {
+    var url =
+        Uri.parse('http://rap2api.taobao.org/app/mock/316903/api/chat/list');
+    var response = await http.get(url);
+    print('Response status: ${response.statusCode}');
+    print('Response body: ${response.body}');
+    print("Response body的类型:  ${response.body is String}");
+
+    if (response.statusCode == 200) {
+      final res = json.decode(response.body);
+      print(res['lists']);
+
+      List<Chat> lists =
+          res['lists'].map<Chat>((el) => Chat.fromMap(el)).toList();
+      return lists;
+    } else {
+      throw Exception("statusCode: ${response.statusCode}");
+    }
+
+    // final chatMap = {'chat_name': 'wxx', 'message': '123141', 'chat_url': '123123'};
+    // print(chatMap);
+    // final chatJson = json.encode(chatMap); // 转字符串
+    // print(chatJson is String);
+    // final chatMap1 = json.decode(chatJson);
+    // print(chatMap1 is Map);
+
+    // final chatModel = Chat.fromMap(chatMap1);
+    // print(chatModel);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    // 获取网络数据
+    geDatas().then((value) {
+      print("1 $value");
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-          alignment: const Alignment(0, 0),
-          child: Text(
-            '微信页面',
-            style: TextStyle(
-              color: Colors.red[300],
-              fontSize: 24,
-            ),
+      body: Container(
+        alignment: const Alignment(0, 0),
+        child: Text(
+          '微信页面',
+          style: TextStyle(
+            color: Colors.red[300],
+            fontSize: 24,
           ),
         ),
-        appBar: AppBar(
-          title: const Text(
-            '微信',
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 26.0,
-              fontWeight: FontWeight.w800,
-            ),
+      ),
+      appBar: AppBar(
+        backgroundColor: weChatThemeColor,
+        centerTitle: true,
+        title: const Text(
+          '微信',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 26.0,
+            fontWeight: FontWeight.w800,
           ),
-          backgroundColor: Colors.grey[350],
         ),
-        backgroundColor: Colors.amber[200]);
+        actions: [
+          PopupMenuButton(
+              color: Color.fromRGBO(0, 0, 0, 0.5),
+              offset: Offset(0, 40), //40这个值是我实验出来的实际上是要算的
+              child: const Icon(
+                IconFont.icon_tianjia,
+                color: Color.fromRGBO(33, 33, 33, 0.65),
+                size: 22,
+              ),
+              onSelected: (value) {},
+              itemBuilder: (BuildContext context) {
+                return <PopupMenuEntry<dynamic>>[
+                  PopupMenuItem(
+                      onTap: () {
+                        print('点击了');
+                      },
+                      child: const Text(
+                        '发起群聊',
+                        style: TextStyle(color: Colors.white),
+                      )),
+                  const PopupMenuItem(
+                      child: Text(
+                    '添加好友',
+                    style: TextStyle(color: Colors.white),
+                  )),
+                  const PopupMenuItem(
+                      child: Text(
+                    '扫一扫',
+                    style: TextStyle(color: Colors.white),
+                  )),
+                  const PopupMenuItem(
+                      child: Text(
+                    '收款码',
+                    style: TextStyle(color: Colors.white),
+                  )),
+                ];
+              })
+        ],
+      ),
+      // backgroundColor: weChatThemeColor,
+    );
   }
 }
