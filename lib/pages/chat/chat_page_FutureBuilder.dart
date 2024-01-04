dbc76362208bc4b68d473290f24769aa9c06faf3
wangxing <xing.wang@xforwardai.com>
2024-01-04 16:21:59 +0800

    fix: 网络请求处理

--- lib/pages/chat/chat_page_FutureBuilder.dart
+++ lib/pages/chat/chat_page_FutureBuilder.dart
@@ -3,7 +3,6 @@ import 'dart:ffi';
 import 'package:flutter/material.dart';
 import 'package:flutter_vx/common/iconfont.dart';
 import 'package:flutter_vx/const.dart';
-import 'package:flutter_vx/pages/friends/friend_data.dart';
 import 'package:http/http.dart' as http;
 
 class Chat {
@@ -30,113 +29,92 @@ class ChatPage extends StatefulWidget {
 }
 
 class _ChatPageState extends State<ChatPage> {
-  bool _loading = false;
-  bool _hasError = false;
-  List<Chat> _datas = [];
-  bool _netWork = false;
-
-  bool _timeout = false;
-
   Future<List<Chat>> geDatas() async {
-    _timeout = false;
-    setState(() {
-      _loading = true;
-      _hasError = false;
-      _netWork = false;
-    });
     var url =
         Uri.parse('http://rap2api.taobao.org/app/mock/316903/api/chat/list');
     var response = await http.get(url);
+    print('Response status: ${response.statusCode}');
+    print('Response body: ${response.body}');
+    print("Response body的类型:  ${response.body is String}");
 
     if (response.statusCode == 200) {
       final res = json.decode(response.body);
+      print(res['lists']);
+
       List<Chat> lists =
           res['lists'].map<Chat>((el) => Chat.fromMap(el)).toList();
       return lists;
     } else {
       throw Exception("statusCode: ${response.statusCode}");
     }
+
+    // final chatMap = {'chat_name': 'wxx', 'message': '123141', 'chat_url': '123123'};
+    // print(chatMap);
+    // final chatJson = json.encode(chatMap); // 转字符串
+    // print(chatJson is String);
+    // final chatMap1 = json.decode(chatJson);
+    // print(chatMap1 is Map);
+
+    // final chatModel = Chat.fromMap(chatMap1);
+    // print(chatModel);
   }
 
   @override
   void initState() {
     super.initState();
     // 获取网络数据
-    geDatas()
-        .then((List<Chat> datas) {
-          if (!_timeout) {
-            setState(() {
-              _loading = false;
-              _hasError = false;
-              _datas = datas;
-            });
-          }
-        })
-        .catchError((e) {
-          if (!_timeout) {
-            setState(() {
-              _hasError = true;
-            });
-          }
-          // 错误
-          print(e);
-        })
-        .whenComplete(() {
-          print('执行到了  完毕');
-        })
-        .timeout(const Duration(seconds: 10))
-        // .timeout(const Duration(microseconds: 10))
-        .catchError((timeout) {
-          _timeout = true;
-          setState(() {
-            _netWork = true;
-            _loading = false;
-            _hasError = false;
-            _datas = [];
-          });
-          print("超时 $timeout ");
-        });
+    // geDatas().then((value) {
+    //   print("1 $value");
+    // });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-          child: _loading
-              ? const Text('loading...')
-              : _hasError
-                  ? const Text('出错了')
-                  : _netWork
-                      ? const Text('网络不好')
-                      : _datas.isEmpty //_datas.length == 0;
-                          ? const Text('空页面站位')
-                          : ListView.builder(
-                              itemBuilder: (BuildContext context, int index) {
-                                var item = _datas[index];
-                                return ListTile(
-                                  title: Text(item.name),
-                                  subtitle: Container(
-                                    margin: const EdgeInsets.only(top: 8),
-                                    height: 20,
-                                    child: Text(
-                                      item.message,
-                                      overflow: TextOverflow.ellipsis,
-                                    ),
-                                  ),
-                                  leading: Container(
-                                    width: 44,
-                                    height: 44,
-                                    decoration: BoxDecoration(
-                                      borderRadius: BorderRadius.circular(6),
-                                      image: DecorationImage(
-                                        image: NetworkImage(item.imageUrl),
-                                      ),
-                                    ),
-                                  ),
-                                );
-                              },
-                              itemCount: _datas.length,
-                            )),
+        child: FutureBuilder(
+          future: geDatas(),
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            // print("snapshot_ ${snapshot.data}");
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              // 网络请求中的时候
+              return Container(
+                child: const Text('loading... '),
+              );
+            }
+            // 加载到了数据
+            return ListView(
+              children: snapshot.data.map<Widget>((Chat item) {
+                return ListTile(
+                  title: Text(item.name),
+                  subtitle: Container(
+                    margin: const EdgeInsets.only(top: 8),
+                    height: 20,
+                    child: Text(
+                      item.message,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ),
+
+                  leading: Container(
+                    width: 44,
+                    height: 44,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(6),
+                      image: DecorationImage(
+                        image: NetworkImage(item.imageUrl),
+                      ),
+                    ),
+                  ),
+                  // leading: CircleAvatar(
+                  //   backgroundImage: NetworkImage(item.imageUrl),
+                  // ),
+                );
+              }).toList(),
+            );
+          },
+        ),
+      ),
       appBar: AppBar(
         backgroundColor: weChatThemeColor,
         centerTitle: true,
